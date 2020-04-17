835c29c53a7a8805dd9a0125931e8237a1f71eab
Arunim Joarder <44579152+ArunimJoarder@users.noreply.github.com>
2020-04-17 12:47:00 +0530

    Update Billi.m

--- MATLAB/Billi.m
+++ MATLAB/Billi.m
@@ -43,25 +43,25 @@
 			obj.frontLeft.startPoint= obj.body.orient*[l/2;b/2;0;1];
 			obj.backRight.startPoint= obj.body.orient*[-l/2;-b/2;0;1];
 			obj.backLeft.startPoint= obj.body.orient*[-l/2;b/2;0;1];                       
-    end        
-    function [obj] = update(obj,fr,fl,br,bl,com,angle)%object,frontRight,frontLeft,backRight,backLeft,centre of mass,heading angle
-        obj.body.orient =T(com)*rZ(angle);  
-        
-        l=obj.body.length;
-        b=obj.body.breadth;
-        obj.frontRight.startPoint=obj.body.orient*[l/2;-b/2;0;1];
-        obj.frontLeft.startPoint= obj.body.orient*[l/2;b/2;0;1];
-        obj.backRight.startPoint= obj.body.orient*[-l/2;-b/2;0;1];
-        obj.backLeft.startPoint= obj.body.orient*[-l/2;b/2;0;1];
-        obj.frontRight.endPoint=fr;
-        obj.frontLeft.endPoint= fl;
-        obj.backRight.endPoint= br;
-        obj.backLeft.endPoint= bl;
-        [~, obj.frontRight.joint, ~]=findleg(obj.frontRight.startPoint(1:3,1),obj.frontRight.endPoint,obj.frontRight.l1,obj.frontRight.l2,pi/2);
-        [~, obj.frontLeft.joint, ~]= findleg(obj.frontLeft.startPoint(1:3,1),obj.frontLeft.endPoint,obj.frontLeft.l1,obj.frontLeft.l2,-pi/2);
-        [~, obj.backRight.joint, ~]= findleg(obj.backRight.startPoint(1:3,1),obj.backRight.endPoint,obj.backRight.l1,obj.backRight.l2,pi/2);
-        [~, obj.backLeft.joint, ~]= findleg(obj.backLeft.startPoint(1:3,1),obj.backLeft.endPoint,obj.backLeft.l1,obj.backLeft.l2,-pi/2);
+    		end        
+    		function [obj] = update(obj,fr,fl,br,bl,com,angle)%object,frontRight,frontLeft,backRight,backLeft,centre of mass,heading angle
+			obj.body.orient =T(com)*rZ(angle);  
+
+			l=obj.body.length;
+			b=obj.body.breadth;
+			obj.frontRight.startPoint=obj.body.orient*[l/2;-b/2;0;1];
+			obj.frontLeft.startPoint= obj.body.orient*[l/2;b/2;0;1];
+			obj.backRight.startPoint= obj.body.orient*[-l/2;-b/2;0;1];
+			obj.backLeft.startPoint= obj.body.orient*[-l/2;b/2;0;1];
+			obj.frontRight.endPoint=fr;
+			obj.frontLeft.endPoint= fl;
+			obj.backRight.endPoint= br;
+			obj.backLeft.endPoint= bl;
+			[~, obj.frontRight.joint, ~]=findleg(obj.frontRight.startPoint(1:3,1),obj.frontRight.endPoint,obj.frontRight.l1,obj.frontRight.l2,pi/2);
+			[~, obj.frontLeft.joint, ~]= findleg(obj.frontLeft.startPoint(1:3,1),obj.frontLeft.endPoint,obj.frontLeft.l1,obj.frontLeft.l2,-pi/2);
+			[~, obj.backRight.joint, ~]= findleg(obj.backRight.startPoint(1:3,1),obj.backRight.endPoint,obj.backRight.l1,obj.backRight.l2,pi/2);
+			[~, obj.backLeft.joint, ~]= findleg(obj.backLeft.startPoint(1:3,1),obj.backLeft.endPoint,obj.backLeft.l1,obj.backLeft.l2,-pi/2);
             
-    end
+    		end
 	end
 end
